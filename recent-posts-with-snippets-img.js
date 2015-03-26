004dba3bc08dee1f426ba980a1eaa8a0c770dce4
hoandqt <hoandqt@gmail.com>
2015-03-26 13:38:06 +0000

    latest posts with image, title, summary

--- recent-posts-with-snippets-img.js
+++ recent-posts-with-snippets-img.js
@@ -4,10 +4,142 @@
 // Mod by MyDigitalLemon | Edit by Hoa Dang Nguyen
 // Original by blogsolute.com | blogsudo.com
 // ----------------------------------------
+// ----------------------------------------
+// SHOW RECENT POSTS
+// ----------------------------------------
+// Mod by MyDigitalLemon | Edit by Hoa Dang Nguyen
+// Original by blogsolute.com | blogsudo.com
+// ----------------------------------------
 function showrecentposts_img_www(json) {
   for (var i = 0; i < numposts_img; i++) {
     var details = first_part(json,i);
     var container = document.getElementById('www-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_chuyenla(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('chuyenla-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_tinthethao(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('tinthethao-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_giaitri(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('giaitri-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_gaixinh(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('gaixinh-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_congnghe(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('congnghe-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_khoahoc(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('khoahoc-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_dulich(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('dulich-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_docnhat(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('docnhat-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_suckhoe(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('suckhoe-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_doisong(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('doisong-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_anh(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('anh-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_thucpham(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('thucpham-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_cuoi(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('cuoi-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_yeu(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('yeu-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_cuocsong(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('cuocsong-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_hoctap(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('hoctap-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_qua(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('qua-img'); 
+    last_part(details,container);
+  }
+}
+function showrecentposts_img_chuabiet(json) {
+  for (var i = 0; i < numposts_img; i++) {
+    var details = first_part(json,i);
+    var container = document.getElementById('chuabiet-img'); 
     last_part(details,container);
   }
 }
