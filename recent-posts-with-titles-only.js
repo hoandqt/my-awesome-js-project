3756379058b7dd3311c4cc00c324a5accaf79889
hoandqt <hoandqt@gmail.com>
2015-03-26 09:09:21 +0000

    test

--- recent-posts-with-titles-only.js
+++ recent-posts-with-titles-only.js
@@ -1,8 +1,7 @@
 // ----------------------------------------
-// SHOW RECENT POSTS 2 
+// SHOW RECENT POSTS 
 // ----------------------------------------
 function showrecentposts(json) {
-//if (www == 'www') {
   for (var i = 0; i < numposts; i++) {
     var entry = json.feed.entry[i];
     var posttitle = entry.title.$t;
@@ -25,5 +24,52 @@ function showrecentposts(json) {
     div.appendChild(span);
     container.appendChild(div);
   }
-//}
+}
+function showrecentposts_chuabiet(json) {
+  for (var i = 0; i < numposts; i++) {
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    if (i == json.feed.entry.length) break;
+    for (var k = 0; k < entry.link.length; k++) {
+        if (entry.link[k].rel == 'alternate') {
+            posturl = entry.link[k].href;
+            break;
+        }
+    }
+    posttitle = posttitle.link(posturl);
+    
+    var container = document.getElementById('chuabiet'); 
+
+    var div = document.createElement("div");
+    div.className = "bbrecpost2";
+    var span = document.createElement("span");
+    span.innerHTML = posttitle;
+    div.appendChild(span);
+    container.appendChild(div);
+  }
+}
+function showrecentposts_congiap(json) {
+  for (var i = 0; i < numposts; i++) {
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    if (i == json.feed.entry.length) break;
+    for (var k = 0; k < entry.link.length; k++) {
+        if (entry.link[k].rel == 'alternate') {
+            posturl = entry.link[k].href;
+            break;
+        }
+    }
+    posttitle = posttitle.link(posturl);
+    
+    var container = document.getElementById('congiap'); 
+
+    var div = document.createElement("div");
+    div.className = "bbrecpost2";
+    var span = document.createElement("span");
+    span.innerHTML = posttitle;
+    div.appendChild(span);
+    container.appendChild(div);
+  }
 }
