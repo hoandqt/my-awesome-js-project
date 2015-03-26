777e8df7ec927ae12a2037a937367b41b1554b70
hoandqt <hoandqt@gmail.com>
2015-03-26 14:50:30 +0000

    fix break

--- recent-posts-with-titles-only.js
+++ recent-posts-with-titles-only.js
@@ -516,7 +516,7 @@ function title_first_part(json,i) {
     var entry = json.feed.entry[i];
     var posttitle = entry.title.$t;
     var posturl;
-    if (i == json.feed.entry.length) break;
+    if (i == json.feed.entry.length) return;
     for (var k = 0; k < entry.link.length; k++) {
         if (entry.link[k].rel == 'alternate') {
             posturl = entry.link[k].href;
